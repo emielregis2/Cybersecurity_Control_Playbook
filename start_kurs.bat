dd625d3037cdf6f56b79701b7912a0fff0730033
emielregis2 <dariusz.gasior@gmail.com>
2026-06-02 13:29:42 +0200

    fix: fetch przez localhost:8080, dodano start_kurs.bat

---
+++ start_kurs.bat
@@ -0,0 +1,16 @@
+@echo off
+echo Uruchamianie kursu Cybersecurity Control Playbook...
+
+:: Zabij stary serwer jesli dziala
+for /f "tokens=5" %%a in ('netstat -aon ^| findstr ":8080 "') do taskkill /F /PID %%a >nul 2>&1
+
+:: Uruchom serwer HTTP w tle
+start /B python -m http.server 8080
+
+:: Odczekaj chwile
+timeout /t 2 /nobreak >nul
+
+:: Otworz w przegladarce
+start http://localhost:8080/kurs-cyberbezpieczenstwo.html
+
+echo Kurs dostepny pod: http://localhost:8080/kurs-cyberbezpieczenstwo.html
